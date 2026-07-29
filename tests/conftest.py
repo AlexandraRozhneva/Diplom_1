c710b2413a797ba2ebd1343e0f4a15a28cddf615
Alexandra Rozhneva <wolchica27@yandex.ru>
2026-07-29 11:49:50 +0300

    conftest.py - фикстуры

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,6 @@
-import sys
-import os
-from pathlib import Path
-
-# Добавляем корневую директорию в PYTHONPATH
-sys.path.insert(0, str(Path(__file__).parent.parent))
-
 import pytest
 from unittest.mock import Mock
-from burger import Burger
+from praktikum.burger import Burger
 
 
 @pytest.fixture
